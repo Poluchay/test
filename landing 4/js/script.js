84f2d22542e5b08e1fe713e7736bbb89509b52d1
Poluchay <Orionp1997@mail.ru>
2021-03-22 20:02:46 +0700

    Add files via upload

---
+++ landing 4/js/script.js
@@ -0,0 +1,33 @@
+
+//     let a = document.querySelector('.header__burger');
+//     a.addEventListener('click', ()=>{
+//         document.querySelector('.nav').classList.toggle('view');
+//         alert('Kolya huy');
+//     });
+
+//     let div = document.querySelector('.huy');//нашли див по классу
+//     let input = document.querySelector('.huy1'); //нашли инпут по классу
+
+//     let v = function runTime (){ //Если нажали на див
+
+//   let timerNumber = 0; // Таймер = 0 до функции чтобы повторно запустить 
+//   //таймер с нуля
+//   let interval = setInterval(() =>{  //функция с таймером работает 
+//     //пока работает таймер, каждую секунду вызывает сама себя
+//     div.innerHTML = timerNumber; // помещаем в див таймер
+//     timerNumber = timerNumber + 1; // таймер 0 + 1, 1 + 1.....
+//     if(timerNumber == 12){
+//       clearInterval(interval);
+//     }
+//   }, 1000)
+// };
+
+// div.addEventListener('click', v);
+
+// const a = new Array (1,2,3,4, v = new Function('b'));
+// console.log(a)
+// console.log(a.toString())
+// const Arr = new Object ()
+// console.log(Arr)
+//window.innerWidth = 300;
+//window.outerWidth = 300;
