a1500f4e0276426d096bd4abc410ac17ce0c65c2
Poluchay <Orionp1997@mail.ru>
2021-07-15 15:44:07 +0700

    Add files via upload

--- LitvinUlia/js/app.js
+++ LitvinUlia/js/app.js
@@ -478,6 +478,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
    });
 
+   //mask
+   function mask(event) {
+      let matrix = "+7 (___) ___ ____",
+         i = 0,
+         def = matrix.replace(/\D/g, ""),
+         val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, function (a) {
+         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+      if (event.type == "blur") {
+         if (this.value.length == 2) this.value = ""
+      } else setCursorPosition(this.value.length, this)
+   };
+   let input = document.querySelector("#tel");
+   if (input) {
+      input.addEventListener("input", mask, false);
+   }
+
+
 })
 // ('files/regular.js', {})
 // ('files/forms.js', {})
