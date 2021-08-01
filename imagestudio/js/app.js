de7fa8ca0cc7993724a1f37045786c90ad2acaca
Poluchay <Orionp1997@mail.ru>
2021-08-01 22:02:04 +0700

    Add files via upload

--- imagestudio/js/app.js
+++ imagestudio/js/app.js
@@ -728,6 +728,11 @@ if (window.innerWidth >= 768) {
 }
 document.addEventListener('DOMContentLoaded', () => {
 
+   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+   let vh = window.innerHeight * 0.01;
+   // Then we set the value in the --vh custom property to the root of the document
+   document.documentElement.style.setProperty('--vh', `${vh}px`);
+
    document.addEventListener('click', documentActions);
    // Actions (делигирование события)
    function documentActions(e) {
